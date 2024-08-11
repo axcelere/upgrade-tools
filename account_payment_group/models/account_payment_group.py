3a33147d78e9100473dd14d92195e9e7294022a3
Rafael Aguero Baquero <raguero@quanam.com>
2024-08-11 07:14:04 -0300

    [FIX]

--- account_payment_group/models/account_payment_group.py
+++ account_payment_group/models/account_payment_group.py
@@ -8,8 +8,5 @@ from odoo.exceptions import ValidationError
 class AccountPaymentGroup(models.Model):
     _name = "account.payment.group"
     _description = "Payment Group"
-    _order = "payment_date desc, name desc"
-    _inherit = 'mail.thread'
-    _check_company_auto = True
 
     name = fields.Char(string='Number', readonly=True, copy=False)

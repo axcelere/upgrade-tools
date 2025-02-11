4ea020c419f577aef89774b0f8215f69d872fe75
Rafael Aguero Baquero <raguero@quanam.com>
2025-02-11 00:16:14 -0300

    [MIG] pre_init_hook

--- l10n_latam_check_adhoc/__init__.py
+++ l10n_latam_check_adhoc/__init__.py
@@ -3,7 +3,14 @@
 # directory
 ##############################################################################
 from . import models
+import logging
+_logger = logging.getLogger(__name__)
 
 def _pre_init_method(env):
+    _logger.info('L10n Latam Check Adhoc Executing pre init method')
+    env.cr.execute('''
+        ALTER TABLE account_payment_method
+        DROP CONSTRAINT IF EXISTS account_payment_method_name_code_unique;
+    ''')
     for rec in env['account.payment.method'].search([]):
         rec.write({'code': '%s-%s' % (rec.code, 'old')})

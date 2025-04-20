a1d75f274abe30618cf43ed475c85aa4e171de1c
Rafael Aguero Baquero <raguero@quanam.com>
2025-04-20 15:31:38 -0300

    [MIG] empty addons

---
+++ l10n_ar_account_withholding/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Argentina - Payment Withholdings - FIXES',
+    'version': "18.0.1.0.0",
+    'description': """Allows to register withholdings during the payment of an invoice.""",
+    'author': 'ADHOC SA',
+    'countries': ['ar'],
+    'category': 'Accounting/Localizations',
+    'depends': [],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}

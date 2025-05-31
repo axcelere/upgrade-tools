ea215bed6de61ff5cefc9d8d66be1466964b98d4
Rafael Aguero Baquero <raguero@quanam.com>
2025-05-31 11:14:52 -0300

    [UPD] Refactor fix_checks_ofupgrade_15 method to improve SQL queries and use check_number for naming

--- upgrade_fix_check/__init__.py
+++ upgrade_fix_check/__init__.py
@@ -12,14 +12,28 @@ class FixCheck(models.Model):
     def fix_checks_ofupgrade_15(self):
         AccountPayment = self.env['account.payment'].sudo()
         Check = self.env['l10n_latam.check'].sudo()
+        # select COUNT(*) from account_payment where journal_id IN (22,15) and check_number_moved0 <> '0' AND payment_type = 'inbound';
         self.env.cr.execute(
-            """SELECT account_payment.id FROM account_payment, account_payment_method WHERE account_payment.payment_type = 'inbound' AND check_number_moved0 <> 0 and account_payment.payment_method_id = account_payment_method.id AND account_payment_method.code = 'new_third_party_checks'""",
+            """SELECT
+    p.id
+FROM
+    account_payment p,
+    account_payment_method_line pml,
+    account_payment_method pm
+WHERE
+    p.journal_id IN (22, 15)
+    AND (p.check_number_moved0 IS NOT NULL OR p.check_number_moved0 <> '0')
+    AND p.payment_type = 'inbound'
+    AND p.payment_method_line_id = pml.id
+    AND pml.payment_method_id = pm.id
+""",
         )
         ids = self.env.cr.fetchall()
         old_checks = AccountPayment.browse([x[0] for x in ids])
         for old_check in old_checks:
+            query = """select check_number_moved0 from account_payment where id = %s""" % (old_check.id, )
             Check.create({
-                'name': old_check.name,
+                'name': old_check.check_number,
                 'amount': old_check.amount,
                 'payment_id': old_check.id,
                 'payment_date': old_check.date,
@@ -27,13 +41,23 @@ class FixCheck(models.Model):
 
         # CHEQUES PROPIOS
         self.env.cr.execute(
-            """SELECT account_payment.id FROM account_payment, account_payment_method WHERE check_number_moved0 <> 0 and account_payment.payment_method_id = account_payment_method.id AND account_payment_method.code = 'check_printing'""",
+            """SELECT
+    p.id
+FROM
+    account_payment p,
+    account_payment_method_line pml,
+    account_payment_method pm
+WHERE
+    (p.check_number_moved0 IS NOT NULL OR p.check_number_moved0 <> '0')
+    AND p.payment_type = 'outbound'
+    AND p.payment_method_line_id = pml.id
+    AND pml.payment_method_id = pm.id AND pm.code='check_printing';""",
         )
         ids = self.env.cr.fetchall()
         old_checks = AccountPayment.browse([x[0] for x in ids])
         for old_check in old_checks:
             Check.create({
-                'name': old_check.name,
+                'name': old_check.check_number,
                 'amount': old_check.amount,
                 'payment_id': old_check.id,
                 'payment_date': old_check.date,

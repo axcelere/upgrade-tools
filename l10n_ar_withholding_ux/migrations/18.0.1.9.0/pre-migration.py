55e3b5b10a1f84a1e369119aba835705d37cffbd
Rafael Aguero Baquero <raguero@quanam.com>
2025-10-18 09:09:03 -0300

    [UPD] upgrade-tools

--- l10n_ar_withholding_ux/migrations/18.0.1.9.0/pre-migration.py
+++ l10n_ar_withholding_ux/migrations/18.0.1.9.0/pre-migration.py
@@ -2,7 +2,7 @@ from odoo import SUPERUSER_ID, api, tools
 import logging
 _logger = logging.getLogger(__name__)
 
-def migrate(env, version):
+def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     view = env.ref("stock_ux.res_config_settings_view_form", raise_if_not_found=False)
     if view:
@@ -11,9 +11,8 @@ def migrate(env, version):
     addon_name = 'l10n_ar_withholding_ux'
     views = env['ir.ui.view'].search([('module', '=', addon_name)])
     if views:
-        logger.info(f"Eliminando {len(views)} vistas del módulo '{addon_name}'.")
+        _logger.info(f"Eliminando {len(views)} vistas del módulo '{addon_name}'.")
         views.unlink()
     else:
-        logger.info(f"No se encontraron vistas para el módulo '{addon_name}'.")
-    env.cr.commit()
-
+        _logger.info(f"No se encontraron vistas para el módulo '{addon_name}'.")
+    cr.commit()
